acc5803314ae3752f6ad1f87c066ca6137c69c4d
EmmaK <emmaculatewkamau@gmail.com>
2022-03-13 15:40:25 +0300

    Add focus and blut to contact form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+// JQuery functions
 $(document).ready(function(){
+    // Hover over portfolio images to get a tooltip with the title
     $("img.portfolio-img").tooltip({
         tooltipClass: "tooltip"
     })
+
+    //
+    $("input").focus(function(){
+        $(this).addClass("form-control");
+      });
+      $("input").blur(function(){
+        $(this).addClass("form-control");
+      });
+
+    // Click on image to display description while hideing the rest of the service icons
     $("img#design").click(function(){
         $('div#part-one').removeClass("centered")
         $('p.service-desc').hide()
@@ -27,3 +39,6 @@ $(document).ready(function(){
         console.log("part three loaded perfectly")
     })
 })
+
+
+//Javascript functions
