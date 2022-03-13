95abbd97015272341e9dd9d1ad7dd9593883cd6f
EmmaK <emmaculatewkamau@gmail.com>
2022-03-13 15:24:27 +0300

    Add functionality to the services icons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-    $(document).tooltip({
-        tooltipClass:"tooltip"
+    $("img.portfolio-img").tooltip({
+        tooltipClass: "tooltip"
+    })
+    $("img#design").click(function(){
+        $('p.service-desc').hide()
+        $('p#design-desc').show()
+        $('div#part-two').addClass("centered")
+        $('div#part-three').addClass("centered")
+        console.log("part one loaded perfectly")
+    })
+    $("img#development").click(function(){
+        $('p.service-desc').hide()
+        $('p#dev-desc').show()
+        $('div#part-one').addClass("centered")
+        $('div#part-three').addClass("centered")
+        console.log("part two loaded perfectly")
+    })
+    $("img#prod-mngmt").click(function(){
+        $('p.service-desc').hide()
+        $('p#prod-desc').show()
+        $('div#part-one').addClass("centered")
+        $('div#part-two').addClass("centered")
+        console.log("part three loaded perfectly")
     })
 })
